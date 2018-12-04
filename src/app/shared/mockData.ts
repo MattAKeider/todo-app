4983778b4fd17ec9983c8fafacea0eef18f756e0
MattAKeider <kderr41@gmail.com>
2018-12-03 22:57:39 -0500

    Change mocked objects to creating new Item objects to increment id

--- src/app/shared/mockData.ts
+++ src/app/shared/mockData.ts
@@ -1,10 +1,10 @@
 import { Item } from './item.model';
 
 export const ITEMS:Item[] = [
-    {contents:'Clean the house'},
-    {contents:'Give dogs a bath'},
-    {contents:'Mow the long'},
-    {contents:'Get truck repaired'},
-    {contents:'Prune trees'},
-    {contents:'Clean windows'}
+    new Item('Clean the house'),
+    new Item('Give dogs a bath'),
+    new Item('Mow the long'),
+    new Item('Get truck repaired'),
+    new Item('Prune trees'),
+    new Item('Clean windows')
 ];
