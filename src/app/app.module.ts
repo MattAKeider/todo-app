05f6788dfe2be60729d747aa565b5d44becaa376
MattAKeider <kderr41@gmail.com>
2019-01-12 12:38:58 -0500

    Add Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { AddItemComponent } from './add-item/add-item.component';
+
+const routes:Routes = [
+  { path: '', component: ListComponent },
+  { path: 'add', component: AddItemComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +20,8 @@ import { AddItemComponent } from './add-item/add-item.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
